fd39ad004c1e42b3b0c629417716fd2751465366
quiz2020-droid <69303923+quiz2020-droid@users.noreply.github.com>
2020-08-06 16:08:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAnPYgE3iikWDwkVxMxQTDIfRi90MFr1Qc",
+  authDomain: "instagram-followers-and-likes.firebaseapp.com",
+  databaseURL: "https://instagram-followers-and-likes.firebaseio.com",
+  projectId: "instagram-followers-and-likes",
+  storageBucket: "instagram-followers-and-likes.appspot.com",
+  messagingSenderId: "7046317116",
+  appId: "1:7046317116:web:db2ec1fdbe57399a07af39",
+  measurementId: "G-MMEXDBZNB7"
 };
 
      
